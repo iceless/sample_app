7dfdc101c07b46c2bd6aede4c4577e5de4428784
Bing W <iceless.bing@gmail.com>
2013-05-06 19:07:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'cf76a42c5703b994002d50470ae6a67990b3db8ae21a28d80e9bdb67ccb577e6b3c217a25c0b56e1df958900d022cf07e60b0f5b1c5bda8d24d29357a4dc4c0e'
